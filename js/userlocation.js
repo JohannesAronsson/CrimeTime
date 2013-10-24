7e404d2ff3a95595b5ea1e88710945b08cfa9863
Daniel Beauchamp <daniel.beauchamp@shopify.com>
2013-10-24 22:45:17 +0200

    Fix marker icon for where you are.
    Try something like this. Turns out mapbox lets you give the html that a marker should have. I just put an image in there....
    
    You might want to adjust the image you use.

--- js/userlocation.js
+++ js/userlocation.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    map.setView([59.314838, 18.069763], 14);
 
-    var userLocation = L.divIcon({newIcon: url="http://t1.gstatic.com/images?q=tbn:ANd9GcRfTku-YRG7QGBu5YN05_CkkaqvRuMPCpGYH0zs5NoY2XhKFcWa"});
+    var userLocation = L.divIcon({html: "<img src='http://t1.gstatic.com/images?q=tbn:ANd9GcRfTku-YRG7QGBu5YN05_CkkaqvRuMPCpGYH0zs5NoY2XhKFcWa'>"});
     var user = L.marker([0, 0], {icon: userLocation}).addTo(map);
     L.mapbox.tileLayer('http://a.tiles.mapbox.com/v3/lisenm.map-0r6yxgzt.json', {
         maxZoom: 18
