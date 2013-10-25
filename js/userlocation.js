f1b2ed4602ff48a1a49afafc03587e1d6ca12886
Sirak Tekeste <sirak.tekeste@hyperisland.se>
2013-10-25 10:33:03 +0200

    Lagt till en egen positionerings marker.

--- js/userlocation.js
+++ js/userlocation.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-   map.setView([59.314838, 18.069763], 14);
+   map.setView([59.314838, 18.069763], 10);
 
-    var userLocation = L.divIcon({html: "<img src='http://t1.gstatic.com/images?q=tbn:ANd9GcRfTku-YRG7QGBu5YN05_CkkaqvRuMPCpGYH0zs5NoY2XhKFcWa'>"});
+    var userLocation = L.divIcon({html: "<img src='https://dl.dropboxusercontent.com/u/4077265/star-stroked-24.svg'>"});
     var user = L.marker([0, 0], {icon: userLocation}).addTo(map);
     L.mapbox.tileLayer('http://a.tiles.mapbox.com/v3/lisenm.map-0r6yxgzt.json', {
         maxZoom: 18
     }).addTo(map);
 
     navigator.geolocation.watchPosition(function(data) {
-        console.log("this shuold be working")
+        console.log("this should be working")
         var lat = data['coords']['latitude'];
         var lng = data['coords']['longitude'];
         var newLatLng = new L.LatLng(lat, lng);
 
-        map.setView(new L.LatLng(lat, lng), 16);
+        map.setView(new L.LatLng(lat, lng), 14);
         
         user.setLatLng(newLatLng);
 
